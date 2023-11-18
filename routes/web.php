710b6d20c33b09369db5e29ae46eb1c1804d5a6b
廖文忠 <wcliao1962@gmail.com>
2023-11-18 22:29:54 +0800

    產生與products 相關的七個resource (CRUD) 路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductController::class);
+/*
+products.index:     GET         /                       ProductController@index
+                    列出所有產品
+products.show:      GET|HEAD    products/{product}      ProductController@show
+                    檢視某一產品
+products.create:    GET         products                ProductController@create
+                    產生產品新增的表單
+products.store:     POST        products                ProductController@store
+                    儲存新增的產品
+products.edit:      GET|HEAD    products/{product}/edit ProductController@edit
+                    產生某一產品修改的表單
+products.update:    PUT|PATCH   products/{product}      ProductController@update
+                    更新某一產品
+products.destroy:   DELETE      products/{product}      ProductController@destroy
+                    刪除某一產品
+*/
