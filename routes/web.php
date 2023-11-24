458237a8b99dce0dad29838570abd7333c4ae634
廖文忠 <wcliao1962@gmail.com>
2023-11-24 22:35:11 +0800

    安裝laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,3 +35,15 @@ products.update:    PUT|PATCH   products/{product}      ProductController@update
 products.destroy:   DELETE      products/{product}      ProductController@destroy
                     刪除某一產品
 */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
