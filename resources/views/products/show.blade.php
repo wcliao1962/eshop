c4876ebc64c8fc6cda16fa0229400d1e617d66c6
廖文忠 <wcliao1962@gmail.com>
2023-12-16 18:16:12 +0800

    在view當中使用asset()以正確取得css & js的URL

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,11 +7,12 @@
     <meta name="author" content="" />
     <title>Shop Item - Start Bootstrap Template</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{asset('assets/favicon.ico')}}" />
+    <!-- Bootstrap icons-->" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
 </head>
 <body>
 <!-- Navigation-->
@@ -180,6 +181,6 @@
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
-<script src="js/scripts.js"></script>
+<script src="{{asset("js/scripts.js")}}"></script>
 </body>
 </html>
